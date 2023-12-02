7afd56d6817848dd5a3caf95f576b263d93fcf3a
Hanif Azhar Ramadhan <95236022+Hnf77@users.noreply.github.com>
2023-12-02 16:37:38 +0700

    Update Custom_Jscrollbar.java

--- src/custom/Custom_Jscrollbar.java
+++ src/custom/Custom_Jscrollbar.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package custom;
 
 import javax.swing.*;
@@ -15,21 +14,3 @@ public class Custom_Jscrollbar extends JScrollBar{
     }
     
 }
-=======
-package custom;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class Custom_Jscrollbar extends JScrollBar{
-    
-    public Custom_Jscrollbar(){
-        setUI(new Setting_ModernScrollBarUI());
-        setPreferredSize(new Dimension(8, 8));
-        setForeground(new Color(48, 144, 216));
-        setBackground(Color.WHITE);
-        
-    }
-    
-}
->>>>>>> 27539dd84cdb24b13bf769e22064416867b8e875
